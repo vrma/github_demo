367a04cf07823643cac80cbdabeb653e005306f4
rubenkiworks <rubengomezkiworks@gmail.com>
2024-10-01 20:00:25 +0200

    para conflictos

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,8 +6,12 @@ package com.example;
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Empleado empleado;
+        empleado = new Empleado();
     }
 }
---
+++ src/main/java/com/example/Empleado.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Empleado {
+    private String nombre;
+    private String ape1;
+    
+    public Empleado(){
+
+    }
+}
